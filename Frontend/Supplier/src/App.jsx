c7e9f6ae8d01e9eb3245e2bf54c1c5fd9bf6be8e
TajulTarek <tarekahmad484@gmail.com>
2025-01-08 23:45:30 +0600

    gitignore updated

--- Frontend/Supplier/src/App.jsx
+++ Frontend/Supplier/src/App.jsx
@@ -1,10 +1,19 @@
 import Orders from "./Orders";
 import SupplierDashboard from "./SupplierDashboard";
+import { BrowserRouter as Router, Route, Routes, Link, BrowserRouter } from 'react-router-dom';
 
 export default function App() {
   return (
     <>
-      <Orders/>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/login" element={<Login />} > </Route> */}
+          <Route path="/" element={<SupplierDashboard />}> </Route>
+          <Route path="/orders" element={<Orders />}></Route>
+          
+          
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
